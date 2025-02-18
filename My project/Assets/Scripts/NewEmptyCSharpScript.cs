8f6f56d7eb1c7ac697c560a541eca7e47b3b6b52
tjsilva257 <163521712+tjsilva257@users.noreply.github.com>
2025-02-18 13:57:35 +0100

    made a basic camera for player

---
+++ My project/Assets/Scripts/NewEmptyCSharpScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    // i give up
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform SwordWarrior;
+    public Rigidbody rigidbody;
+
+    public float rotationSpeed;
+}
