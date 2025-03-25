6b03754d97388d2822d8558a3579a148ca3515ed
tjsilva257 <163521712+tjsilva257@users.noreply.github.com>
2025-03-25 13:42:23 +0100

    made a blend tree for animation

--- My project/Assets/Scripts/PlayerScript/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerScript/PlayerMovement.cs
@@ -3,12 +3,23 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>(); // Get the Animator component
+    }
 
     void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
-        transform.Translate(movement, Space.World);
-    }
+{
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+
+    // Update Animator Parameters
+    animator.SetFloat("VelocityX", horizontal);
+    animator.SetFloat("VelocityY", vertical);
+
+    Vector3 movement = new Vector3(horizontal, 0, vertical) * moveSpeed * Time.deltaTime;
+    transform.Translate(movement, Space.World);
+}
 }
